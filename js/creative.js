37777a11658691bf55b8f58134a8db1a32ed960d
Scott Murray <rai.five@gmail.com>
2017-11-01 14:58:20 +0000

    Initial Commit

---
+++ js/creative.js
@@ -0,0 +1,24 @@
+
+// below code changes highlighted active tab on nav
+
+$(".nav a").on("click", function(){
+   $(".nav").find(".active").removeClass("active");
+   $(this).parent().addClass("active");
+});
+
+// below code adds smooth scrolling between anchors
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
